2c2c80ecbce75774a3d4c1ed340668dce3b07851
sofia747 <sonypankova4@gmain.com>
2026-05-18 11:16:50 +0300

    feat: добавила красивый вывод треугольника Паскаля и закончила программу

--- prog/Pascals_triangle.cpp
+++ prog/Pascals_triangle.cpp
@@ -2,6 +2,21 @@
 #include <vector>
 using namespace std;
 
+void printTriangle(const vector<vector<int>>& triangle) {
+    int n = triangle.size();
+    int maxNum = triangle[n-1][n-1/2];
+    int maxWidth = to_string(maxNum).length();
+    
+    for (int i = 0; i < n; i++) {
+        cout << string((n - i - 1) * (maxWidth + 1), ' ');
+        
+        for (int j = 0; j <= i; j++) {
+            cout << triangle[i][j] << string(maxWidth + 1 - to_string(triangle[i][j]).length(), ' ');
+        }
+        cout << endl;
+    }
+}
+
 int main() {
     int n;
     cout << "Введите количество строк треугольника Паскаля: ";
@@ -15,4 +30,9 @@ int main() {
             triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j];
         }
     }
+cout << "\nТреугольник Паскаля:\n\n";
+    printTriangle(triangle);
+    
+    return 0;
+}
     
