0dce1ee02ed290514578ed1e9678db33217a8a80
sofia747 <sonypankova4@gmain.com>
2026-05-19 11:49:17 +0300

    добавила функцию main и саму задачу

--- prog/Gr.1.cpp
+++ prog/Gr.1.cpp
@@ -1,3 +1,4 @@
+//Дан неориентированный граф. Вывести на экран все вершины, не смежные с данной.
 #include<iostream>
 #include<vector>
 #include<algorithm>
@@ -21,3 +22,34 @@ void find(int v) {
 	cout << endl;
 }
 
+int main() {
+	setlocale(LC_ALL, "RUS");
+	cout << "кол-во вершин n = "; cin >> n;
+	Gr.resize(n);
+	int m;
+	cout << "кол-во ребер m = "; cin >> m;
+	cout << "вводим ребра ";
+	for (int j = 0; j < m; j++) {
+		int x, y;
+		cin >> x >> y;
+
+		Gr[x].push_back(y);
+		Gr[y].push_back(x);
+	}
+
+	for (int i = 0; i < n; i++) {
+		cout << i << " -> ";
+		for (int j = 0; j < Gr[i].size(); j++) {
+			cout << Gr[i][j] << " ";
+		}
+		cout << "| ∅" << endl;
+	}
+	cout << "номер вершины ";
+	int v;
+	cout << "v = "; cin >> v;
+	if (v < 0 || v >= n) {
+		return 1;
+	}
+	find(v);
+	return 0;
+}
